a6633811b6fabaac27f06ec1f1c29eec4330c3f4
DoTienThanh325 <dotienthanh7a2@gmail.com>
2025-10-14 11:16:46 +0700

    lastest

--- src/Database/TourGuide.sql
+++
@@ -1,8 +0,0 @@
--- Bảng trung gian giữa Tour và Guide
-CREATE TABLE TourGuide (
-    tourId VARCHAR(50),
-    guideId VARCHAR(50),
-    PRIMARY KEY (tourId, guideId),
-    FOREIGN KEY (tourId) REFERENCES Tour(tourId),
-    FOREIGN KEY (guideId) REFERENCES Guide(Id)
-);
